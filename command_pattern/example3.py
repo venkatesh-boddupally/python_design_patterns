af115390a6c9f60ff18fc6672fcb9824f048ab5a
venkatesg <venkatesh.boddupally@talentica.com>
2019-06-08 19:36:25 +0530

    added example for command pattern

---
+++ command_pattern/example3.py
@@ -0,0 +1,58 @@
+import abc
+
+
+class Command(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def execute(self):
+        pass
+
+
+class LunchCommand(Command):
+    def __init__(self, lunch):
+        self.lunch = lunch
+
+    def execute(self):
+        self.lunch.make_lunch()
+
+
+class DinnerCommand(Command):
+    def __init__(self, dinner):
+        self.dinner = dinner
+
+    def execute(self):
+        self.dinner.make_dinner()
+
+
+class Lunch:
+    def make_lunch(self):
+        print("Lunch is being made")
+
+
+class Dinner:
+    def make_dinner(self):
+        print("Dinner is being made")
+
+
+class MealInvoker:
+    def __init__(self, command):
+        self.command = command
+
+    def set_command(self, command):
+        self.command = command
+
+    def invoke(self):
+        self.command.execute()
+
+
+if __name__ == '__main__':
+    lunch = Lunch() # receiver
+    command_lunch = LunchCommand(lunch) # concrete command
+
+    dinner = Dinner() # receiver
+    command_dinner = DinnerCommand(dinner) # concrete command
+
+    meal_invoker = MealInvoker(command_lunch); # invoker
+    meal_invoker.invoke()
+
+    meal_invoker.set_command(command_dinner)
+    meal_invoker.invoke()
